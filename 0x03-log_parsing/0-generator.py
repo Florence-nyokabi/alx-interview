98e0234618af2e5a2efc3fd6353ce99834747a57
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2023-01-18 11:51:45 +0300

    Log parsing

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -5,7 +5,8 @@ from time import sleep
 import datetime
 
 for i in range(10000):
-    sleep(random.random())
+    #sleep(random.random())
+    #sleep(0.05)
     sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
         random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
         datetime.datetime.now(),
