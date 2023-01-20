be87ac90bd0e7e9856f17df291c2b86393ac32ea
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2023-01-20 09:27:57 +0300

    Log parsing

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 import random
 import sys
 from time import sleep
--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+
 """ script that reads stdin line by line and computes metrics """
 
 import sys
 
 
-def printsts(dic, size):
+def printStatus(dic, size):
     """ Prints information """
     print("File size: {:d}".format(size))
     for i in sorted(dic.keys()):
@@ -12,8 +13,9 @@ def printsts(dic, size):
             print("{}: {:d}".format(i, dic[i]))
 
 
-sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-       "404": 0, "405": 0, "500": 0}
+# sourcery skip: use-contextlib-suppress
+statusCodes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+               "404": 0, "405": 0, "500": 0}
 
 count = 0
 size = 0
@@ -21,24 +23,24 @@ size = 0
 try:
     for line in sys.stdin:
         if count != 0 and count % 10 == 0:
-            printsts(sts, size)
+            printStatus(statusCodes, size)
 
         stlist = line.split()
         count += 1
 
         try:
             size += int(stlist[-1])
-        except:
+        except Exception:
             pass
 
         try:
-            if stlist[-2] in sts:
-                sts[stlist[-2]] += 1
-        except:
+            if stlist[-2] in statusCodes:
+                statusCodes[stlist[-2]] += 1
+        except Exception:
             pass
-    printsts(sts, size)
+    printStatus(statusCodes, size)
 
 
 except KeyboardInterrupt:
-    printsts(sts, size)
+    printStatus(statusCodes, size)
     raise
